a3e8495125edf141dc675d48685a6674a9884e70
Tamotsu Takahashi <ttakah+github@gmail.com>
2025-12-25 01:53:38 +0900

    Implement narrowing down
    Append ';hint' to preedit to narrow down the conversion
    
    e.g.
    'Kandou' has too many candidates
    'Kandou;aida' converts to one (or a very few) candidate(s)
    'Kandou;kannatu' to (an)other
    
    'Wa' also has too many candidates
    You may want to use 'Wa;heiwa' to narrow them down

--- extension/skk.js
+++ extension/skk.js
@@ -134,6 +134,15 @@ SKK.prototype.systemComplete = function () {
   this.tabbing = 'system';
 };
 
+SKK.prototype.narrowDown = function(entries, hint) {
+  const words = this.dictionary.lookup(hint);
+  if (!words) {
+    return [];
+  }
+  const kanjis = words.data.flatMap((w) => [...w.word]);
+  return entries.filter((e) => kanjis.some((k) => e.word.includes(k)));
+};
+
 SKK.prototype.processRoman = function (key, table, emitter) {
   function isStarting(key) {
     var starting = false;
