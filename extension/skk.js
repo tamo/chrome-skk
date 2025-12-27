106e4d3c21b1f835dfd7a96193092ed7b5f1ef90
Tamotsu Takahashi <ttakah+github@gmail.com>
2025-12-27 18:09:20 +0900

    Use auxiliaryText to show some useful information
    - preedit completion: dictionary type ('user' or 'system')
    - conversion: number of remaining entries (e.g. '+ 10')

--- extension/skk.js
+++ extension/skk.js
@@ -58,11 +58,19 @@ SKK.prototype.updateCandidates = function() {
 
   const candidates = [];
   const noList = this.entries ? this.entries.index <= 2 : true;
-  for (var i = 0; i < (noList ? 3 : 7); i++) {
+  const pageSize = noList ? 3 : 7;
+
+  for (var i = 0; i < pageSize; i++) {
     const start = noList ? 0 : this.entries.index;
     if (start + i >= this.entries.entries.length) {
       break;
     }
+
+    const remaining = Math.max(0, this.entries.entries.length - start - pageSize);
+    if (!this.entries.text || this.entries.text.startsWith('+ ')) {
+      this.entries.text = '+ ' + remaining;
+    }
+
     const entry = this.entries.entries[start + i];
     candidates.push({
       candidate:entry.word,
@@ -71,6 +79,7 @@ SKK.prototype.updateCandidates = function() {
       annotation:entry.annotation
     });
   }
+
   chrome.input.ime.setCandidates({
     contextID:this.context, candidates:candidates
   }).then(() =>
@@ -81,7 +90,9 @@ SKK.prototype.updateCandidates = function() {
         cursorVisible:true,
         vertical:true,
         windowPosition:'composition',
-        pageSize:noList ? 3 : 7
+        pageSize:pageSize,
+        auxiliaryText:this.entries.text,
+        auxiliaryTextVisible:!!this.entries.text
       }
     })
   ).then(() =>
@@ -100,7 +111,7 @@ SKK.prototype.lookup = function(reading, callback) {
   }
 };
 
-SKK.prototype.complete = function(dict_complete) {
+SKK.prototype.complete = function(dict_complete, text) {
   const entries = [];
   if (this.roman.length > 0) {
     for (var k in romanTable) {
@@ -120,7 +131,8 @@ SKK.prototype.complete = function(dict_complete) {
     this.entries = {
       index:3,
       entries:candidates.map((e) => ({word:e})),
-      label:"       "
+      label:"       ",
+      text:text
     };
   } else {
     this.entries = null;
@@ -129,12 +141,12 @@ SKK.prototype.complete = function(dict_complete) {
 };
 
 SKK.prototype.userComplete = function () {
-  this.complete(this.dictionary.userComplete.bind(this.dictionary));
+  this.complete(this.dictionary.userComplete.bind(this.dictionary), 'user');
   this.tabbing = null;
 };
 
 SKK.prototype.systemComplete = function () {
-  this.complete(this.dictionary.systemComplete.bind(this.dictionary));
+  this.complete(this.dictionary.systemComplete.bind(this.dictionary), 'system');
   this.tabbing = 'system';
 };
 
@@ -430,7 +442,8 @@ SKK.prototype.showStatus = function() {
         visible:true,
         cursorVisible:true,
         vertical:true,
-        pageSize:1
+        pageSize:1,
+        auxiliaryTextVisible:false
       }
     })
   ).then(() => {
